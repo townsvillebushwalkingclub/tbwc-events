fdc29ff7fb92957b8b4bf1f541d8b35ae315131c
townsvillebushwalkingclub <townsvillebushwalkingclub@gmail.com>
2025-08-29 11:18:07 +1000

    Add Google Analytics integration to layout for enhanced tracking

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Analytics } from '@vercel/analytics/next'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export const metadata = {
     title: 'Townsville Bushwalking Club - Events Calendar',
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
                 <Analytics />
                 <SpeedInsights />
             </body>
+            <GoogleAnalytics gaId="G-YZJR1FQJW1" />
         </html>
     )
 }
